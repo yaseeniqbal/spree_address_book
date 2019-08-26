7602ba6b41877b2ad21fe72c9ddad0d8c1d8af8a
yaseeniqbal <52909968+yaseeniqbal@users.noreply.github.com>
2019-08-27 01:34:51 +0500

    add route for shipping calculator

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Spree::Core::Engine.add_routes do
   resources :addresses
 
+  get '/shipping_calculator', to: 'addresses#shipping_calculator', as: "shipping_calculator"
+
   if Rails.env.test?
     put '/cart', :to => 'orders#update', :as => :put_cart
   end
